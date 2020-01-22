88cd710c881d01c3337bdb711088d041f23338fd
csadetec <informaticact@santoagostinho.com.br>
2020-01-21 23:43:33 -0300

    Updates

---
+++ precache-manifest.3dcf68a9aede4221324944028df09d8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89197c89229832068fa37cd3dad78291",
+    "url": "/comeve-web/index.html"
+  },
+  {
+    "revision": "73086d211ffe9f1d1239",
+    "url": "/comeve-web/static/css/main.dbd7495c.chunk.css"
+  },
+  {
+    "revision": "5ac9125b64ee5068e460",
+    "url": "/comeve-web/static/js/2.63c79db3.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/comeve-web/static/js/2.63c79db3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "73086d211ffe9f1d1239",
+    "url": "/comeve-web/static/js/main.9b5ae7e9.chunk.js"
+  },
+  {
+    "revision": "865b3fac2b3155c99dd8",
+    "url": "/comeve-web/static/js/runtime-main.560fa575.js"
+  }
+]);
