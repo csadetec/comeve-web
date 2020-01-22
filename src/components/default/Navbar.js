a98c27eae1426fab457600aad730f5a543af2ac1
csadetec <informaticact@santoagostinho.com.br>
2020-01-21 23:53:51 -0300

    teste gh

--- src/components/default/Navbar.js
+++ src/components/default/Navbar.js
@@ -7,7 +7,7 @@ function Navbar(){
   return (
     <header>
       <nav className="navbar navbar-expand-lg navbar-dark indigo mb-4">
-        <a className="navbar-brand" href="/">Comeve</a>
+        <a className="navbar-brand" href="/comeve">Comeve - teste </a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#basicExampleNav"  aria-controls="basicExampleNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
