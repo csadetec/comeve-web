d996eb06c921d2465148b76944e8060f6ff1d1d2
csadetec <informaticact@santoagostinho.com.br>
2020-03-14 15:36:38 -0300

    config api

--- src/service/api.js
+++ src/service/api.js
@@ -2,16 +2,15 @@ import axios from 'axios'
 
 const hostname = () => {
   const app = window.location.hostname 
-  if(app === '10.20.2.22'){
-    return 'http://10.20.2.22:8000/api'
+  if(app === '10.20.0.22'){
+    return 'http://10.20.2.22:8000'
   }
   if(app === 'localhost'){
     return 'http://localhost:8000' 
   }
-  if(app === 'teste-comeve.netlify.com' || app === 'comeve.netlify.com'){
+  if(app === 'comeve-teste.netlify.com' || app === 'comeve.netlify.com'){
     return 'https://comeve-node.azurewebsites.net'
-    //return 'https://comeve-azure.azurewebsites.net/api'
-    //return 'https://comeve.detec.site'
+   
   }
   if(app === 'd-comeve.netlify.com'){
     return 'https://comeve.servico.site/api'
